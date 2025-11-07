6cab1acb86bf27ef26b6ffd389e1708062ee7c9b
nour <nourelimane.alga@grenoble-inp.org>
2025-11-07 15:50:45 +0100

    okk

--- Finding similar items /MinHashing.py
+++ Finding similar items /MinHashing.py
@@ -30,4 +30,9 @@ class Comparesignature :
          pass 
     def CompareSignatures (vec1 , vec2 ):
         assert len(vec1)== len(vec2)
-        return CompareSets().jaccard_similarity(vec1 , vec2)
+        count = 0 
+        for i in range(len(vec1)) :
+            if vec1[i]==vec2[i] :
+                count+=1 
+        return count/len(vec1)
+
